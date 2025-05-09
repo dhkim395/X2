69ab9bfa5848371bb639e203b90b3aff85414394
dhkim395 <dhkim1484@gmail.com>
2025-05-09 10:07:11 +0900

    1차

---
+++ app.mjs
@@ -0,0 +1,24 @@
+import express from "express";
+import postsRouter from "./router/posts.mjs";
+import authRouter from "./router/auth.mjs";
+import { config } from "./config.mjs";
+import {connectDB} from "./db/database.mjs"
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/posts", postsRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+connectDB()
+.then(()=>{
+  app.listen(config.host.port)
+}).catch(console.error)
+
+// db.getConnection().then((connection) => console.log(connection));
+app.listen(config.host.port);
